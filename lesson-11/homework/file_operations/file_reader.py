13efd4243a7779dc53198fd8bd42c58128ce77d6
Bakhytzhan <84700034+ubkhtzhn@users.noreply.github.com>
2025-04-17 11:45:41 +0500

    Create file_reader.py

---
+++ lesson-11/homework/file_operations/file_reader.py
@@ -0,0 +1,6 @@
+def read_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return f.read()
+    except FileNotFoundError:
+        return "File not found!"
