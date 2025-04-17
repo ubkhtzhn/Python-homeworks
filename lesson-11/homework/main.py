14afce06651156d39cb530d1cd534e7c6c2b8db7
Bakhytzhan <84700034+ubkhtzhn@users.noreply.github.com>
2025-04-17 11:40:41 +0500

    Create main.py

---
+++ lesson-11/homework/main.py
@@ -0,0 +1,24 @@
+# python -m venv venv
+
+# venv\Scripts\activate
+
+# pip install requests flask
+
+# pip list
+
+
+
+# main.py
+
+import math_operations as mo
+import string_utils as su
+
+
+print(mo.add(10, 5))        
+print(mo.subtract(10, 5))   
+print(mo.multiply(10, 5))   
+print(mo.divide(10, 0))   
+
+
+print(su.reverse_string("hello")) 
+print(su.count_vowels("hello"))    
